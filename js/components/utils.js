3fec9f7cdb9b218b14fa6c37e7fda72df7daabb7
Maxim <maxtab1@mail.ru>
2025-08-24 01:14:53 +0300

    regular

--- js/components/utils.js
+++ js/components/utils.js
@@ -1,11 +1,11 @@
 export function inputToSpanText() {
 	inputWidth_id.addEventListener("input", () => {
 		// inputWidthText_id.textContent = inputWidth_id.value + " m";
-		roomWidthSpan.textContent = inputWidth_id.value + "m";
+		roomWidthSpan.textContent = "Room X, " + inputWidth_id.value + "m";
 	});
 	inputHeight_id.addEventListener("input", () => {
 		// inputHeightText_id.textContent = inputHeight_id.value + " m";
-		roomHeightSpan.textContent = inputHeight_id.value + "m";
+		roomHeightSpan.textContent = "Room Y, " + inputHeight_id.value + "m";
 	});
 	//
 }
@@ -38,8 +38,9 @@ export function handleFormSubmit() {
 	form_id.addEventListener("reset", () => {
 		setTimeout(() => {
 			//
-			roomWidthSpan.textContent = inputWidth_id.value + "m";
-			roomHeightSpan.textContent = inputHeight_id.value + "m";
+			roomWidthSpan.textContent = "Room X, " + inputWidth_id.value + "m";
+			roomHeightSpan.textContent = "Room Y, " + inputHeight_id.value + "m";
+
 			//
 			const width = parseFloat(inputWidth_id.value);
 			const height = parseFloat(inputHeight_id.value);
