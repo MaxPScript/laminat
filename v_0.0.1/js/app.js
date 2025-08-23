91ed55ea53bc895046caf95fe4c567b47eb896d8
Maxim <maxtab1@mail.ru>
2025-08-23 16:34:53 +0300

    initial

---
+++ v_0.0.1/js/app.js
@@ -0,0 +1,6 @@
+import { inputToSvgText } from "./components/utils.js";
+import { setAspectRatioToSvg } from "./components/utils.js";
+import { handleFormSubmit } from "./components/utils.js";
+inputToSvgText();
+setAspectRatioToSvg();
+handleFormSubmit();
---
+++ v_0.0.1/js/components/utils.js
@@ -0,0 +1,54 @@
+export function inputToSvgText() {
+	inputWidth_id.addEventListener("input", () => {
+		// inputWidthText_id.textContent = inputWidth_id.value + " m";
+		roomWidthSpan.textContent = inputWidth_id.value + "m";
+	});
+	inputHeight_id.addEventListener("input", () => {
+		// inputHeightText_id.textContent = inputHeight_id.value + " m";
+		roomHeightSpan.textContent = inputHeight_id.value + "m";
+	});
+	//
+}
+//
+export function setAspectRatioToSvg() {
+	inputWidth_id.addEventListener("input", () => {
+		const width = parseFloat(inputWidth_id.value);
+		const height = parseFloat(inputHeight_id.value);
+		svg_id.style.aspectRatio = `${width}/${height}`;
+	});
+	inputHeight_id.addEventListener("input", () => {
+		const width = parseFloat(inputWidth_id.value);
+		const height = parseFloat(inputHeight_id.value);
+		svg_id.style.aspectRatio = `${width}/${height}`;
+	});
+}
+//
+export function handleFormSubmit() {
+	form_id.addEventListener("submit", (event) => {
+		event.preventDefault();
+		const btn = event.submitter;
+
+		if (btn.textContent === "Apply") {
+			console.log("Apply logic");
+		} else if (btn.textContent === "Reset") {
+			console.log("Reset logic");
+			form_id.reset();
+		}
+	});
+	form_id.addEventListener("reset", () => {
+		setTimeout(() => {
+			// console.log("Username after reset:", form_id.inputWidth.value);
+			// inputWidthText_id.textContent = inputWidth_id.value + " m";
+			// inputHeightText_id.textContent = inputHeight_id.value + " m";
+			//
+			roomWidthSpan.textContent = inputWidth_id.value + "m";
+			roomHeightSpan.textContent = inputHeight_id.value + "m";
+
+			//
+			const width = parseFloat(inputWidth_id.value);
+			const height = parseFloat(inputHeight_id.value);
+			svg_id.style.aspectRatio = `${width}/${height}`;
+			// svg_id.style.aspectRatio = `${width}/${height}`;
+		}, 0);
+	});
+}
